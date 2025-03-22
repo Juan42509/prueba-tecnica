4e6870881276dd5377294ac6573970076a06306e
Juan Maldonado <juanfer42509@gmail.com>
2025-03-22 16:43:39 -0500

    Se agregan los controladores

---
+++ backend/playList/src/main/java/com/playlist/controller/AuthController.java
@@ -0,0 +1,68 @@
+package com.playlist.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.playlist.configuration.JwtUtil;
+import com.playlist.entity.JwtRequest;
+import com.playlist.entity.JwtResponse;
+import com.playlist.entity.Usuario;
+import com.playlist.exceptions.UsuarioNotFoundException;
+import com.playlist.serviceImpl.UserDetailsServiceImpl;
+
+@RestController
+@RequestMapping("*")
+public class AuthController {
+	
+    
+    @Autowired
+    private AuthenticationManager anthenticationManager;
+    
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    
+    @Autowired
+    private JwtUtil jwtUtils;
+    
+    @PostMapping("/generate-token")
+    public ResponseEntity<?> generarToken(@RequestBody JwtRequest jwtRequest) throws Exception{
+        try {
+            autenticar(jwtRequest.getUsername(), jwtRequest.getPassword());
+        } catch (UsuarioNotFoundException e) {
+            e.printStackTrace();
+            throw new Exception("Usuario no encontrado");
+        }
+        
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(jwtRequest.getUsername());
+        String token = this.jwtUtils.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+    
+    private void autenticar(String username, String password) throws Exception{
+        try {
+            anthenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,password));
+        } catch (DisabledException e) {
+            throw new Exception("Usuario Deshabilitado " + e.getMessage());
+        }catch(BadCredentialsException b){
+            throw new Exception("Credenciales Invalidas " + b.getMessage());
+        }
+    }
+    
+    @GetMapping("/actual-usuario")
+    public Usuario ObtenerUsuarioActual(Principal principal){
+        return (Usuario) this.userDetailsService.loadUserByUsername(principal.getName());
+    }
+    
+}
---
+++ backend/playList/src/main/java/com/playlist/controller/ListaReproduccionController.java
@@ -0,0 +1,67 @@
+package com.playlist.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.playlist.entity.Cancion;
+import com.playlist.entity.ListaReproduccion;
+import com.playlist.service.ListaReproduccionService;
+
+@RestController
+@RequestMapping("/lists")
+@CrossOrigin("*")
+public class ListaReproduccionController {
+	
+	@Autowired
+    private ListaReproduccionService listaService;
+
+    @PostMapping
+    public ResponseEntity<ListaReproduccion> crearLista(@Valid @RequestBody ListaReproduccion lista) {
+        ListaReproduccion nuevaLista = listaService.crearLista(lista);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaLista);
+    }
+    
+    @PostMapping("/{listaId}/add-song")
+    public ResponseEntity<?> agregarCancion(@Valid @PathVariable Long listaId, @RequestBody Cancion cancion) {
+        Optional<Cancion> CancionList = listaService.agregarCancion(listaId, cancion);
+        return ResponseEntity.ok(CancionList);
+    }
+
+    @GetMapping
+    public List<ListaReproduccion> obtenerListas() {
+        return listaService.obtenerListas();
+    }
+
+    @GetMapping("/{listName}")
+    public ResponseEntity<Optional<ListaReproduccion>> obtenerListaByNombre(@PathVariable String listName) {
+    	Optional<ListaReproduccion> lista = listaService.obtenerListaPorNombre(listName);
+    	if(!lista.isPresent()) {
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    	}
+    	
+        return ResponseEntity.ok(listaService.obtenerListaPorNombre(listName));
+    }
+
+    @DeleteMapping("/{listName}")
+    public ResponseEntity<Void> eliminarLista(@PathVariable String listName) {
+        listaService.eliminarLista(listName);
+        return ResponseEntity.noContent().build();
+    }
+    
+    
+}
+
